a4ddf93a304c0e20b4ce950e7b484eb17fdbd8e5
Tom Xue <tomxue0126@gmail.com>
2013-03-05 23:51:44 +0800

    to show what is Python class, __name__, __main__, self and how to call class's function

--- test01.py
+++ test01.py
@@ -1,41 +1,59 @@
-# can play once
+# can play repeatly
 # from link: http://idongnou.com/2012/02/fristpython/
 
 import random
 
-computer = random.randint(1,4)
-user = int(raw_input('Please input a number (1-4) ====> :'))
+class touzi:
 
-if user == 1 :
-    if computer == 2 :
-        print "The Winner is computer!!"
-    elif computer == 4 :
-        print "The winner is user !!"
-    else :
-        print "Draw!!"
+     def u1(self):
+          self.num = random.randint(1,4)
+          if self.num == 1 or self.num == 3:
+               print "User:%s \t Computer:%s \n Draw!" % (str,self.num)
+          elif self.num == 2:
+               print "User:%s \t Computer:%s \n You lose !" % (str,self.num)
+          else :
+               print "User:%s \t Computer:%s \n You win !" % (str,self.num)
+             
+     def u2(self):
+          self.num = random.randint(1,4)
+          if self.num == 2 or self.num == 4:
+               print "User:%s \t Computer:%s \n Draw!" % (str,self.num)
+          elif self.num == 3:
+               print "User:%s \t Computer:%s \n You lose !" % (str,self.num)
+          else :
+               print "User:%s \t Computer:%s \n You win !" % (str,self.num)
+             
+     def u3(self):
+          self.num = random.randint(1,4)
+          if self.num == 1 or self.num == 3:
+               print "User:%s \t Computer:%s \n Draw!" % (str,self.num)
+          elif self.num == 4:
+               print "User:%s \t Computer:%s \n You lose !" % (str,self.num)
+          else :
+               print "User:%s \t Computer:%s \n You win !" % (str,self.num)
+             
+     def u4(self):
+          self.num = random.randint(1,4)
+          if self.num == 2 or self.num == 4:
+               print "User:%s \t Computer:%s \n Draw!" % (str,self.num)
+          elif self.num == 1:
+               print "User:%s \t Computer:%s \n You lose !" % (str,self.num)
+          else :
+               print "User:%s \t Computer:%s \n You win !" % (str,self.num)
 
-elif user == 2 :
-    if computer == 3:
-        print "The winner is computer!!"
-    elif computer == 1:
-        print "The winner is user !!"
-    else:
-        print "Draw!!"
-
-elif user == 3 :
-    if computer == 4:
-        print "The winner is computer!!"
-    elif computer == 2:
-        print "The winner is user!!"
-    else:
-        print "Draw!!"
-
-elif user == 4 :
-    if computer == 1:
-        print "The winner is computer!!"
-    elif computer == 3:
-        print "The winner is user!!"
-    else :
-        print "Draw!!"
-else :
-    print "Sorry! your input is Wrong!!"
+   
+if __name__ == '__main__':
+     p = touzi()
+     while True:
+          str = raw_input('you show: (1/2/3/4/q)')
+          if str == '1':
+               p.u1()
+          elif str == '2':
+               p.u2()
+          elif str == '3':
+               p.u3()
+          elif str == '4':
+               p.u4()
+          else:
+               print "wrong!"
+               break
